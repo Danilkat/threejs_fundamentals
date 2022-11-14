6d6018b3d644e38aedb9cc16847bb536c63f5304
Danil Katashev <katashnya@gmail.com>
2022-11-14 21:36:07 +0700

    renderer.setSize(,,false) example

--- src/pages/p2.js
+++ src/pages/p2.js
@@ -76,5 +76,20 @@ function main() {
   requestAnimationFrame(render);
 }
 
+Split(['#view', '#controls'], {  // eslint-disable-line new-cap
+  sizes: [75, 25],
+  minSize: 100,
+  elementStyle: (dimension, size, gutterSize) => {
+    return {
+      'flex-basis': `calc(${size}% - ${gutterSize}px)`,
+    };
+  },
+  gutterStyle: (dimension, gutterSize) => {
+    return {
+      'flex-basis': `${gutterSize}px`,
+    };
+  },
+});
+
 
 main();
